8a410fe438b4b13b1f5f810323138cd97986a811
viniciusbdzn <viniciusbdzn@gmail.com>
2023-09-15 16:33:59 -0300

    criando projeto conta bancaria

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,8 +1,10 @@
+//importando as classes que serão usadas
 import java.util.Scanner;
 import java.util.Locale;
 
 public class ContaTerminal {
     public static void main(String[] args) {
+        //criando o objeto scanner para o input
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
         System.out.println("Digite o numero da agência: ");
@@ -11,11 +13,13 @@ public class ContaTerminal {
         System.out.println("Digite o numero da conta: ");
         int numeroConta = scanner.nextInt();
 
+        //fechando o scanner
         scanner.close();
         
         String nomeCliente = "Vinicius Badazoni";
         String saldo = "5.320,35";
         
+        //mensagem que será exibida para o usuário
         System.out.println("Olá, Sr(a) " + nomeCliente + ", obrigado por criar uma conta em nosso banco. Sua agência é " + numeroAgencia + " ,conta: " + numeroConta + ".\n Seu saldo disponível é: R$" + saldo);
     }
 }
